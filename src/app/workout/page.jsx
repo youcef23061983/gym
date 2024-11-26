3d4d36d926a24f478019f7eef4fb2fdb0399d273
youcef23061983 <youcef.belcourtois@gmail.com>
2024-11-26 15:54:01 +0100

    env changed commit

--- src/app/workout/page.jsx
+++ src/app/workout/page.jsx
@@ -5,7 +5,7 @@ import React from "react";
 import { options } from "../api/auth/[...nextauth]/options";
 import { redirect } from "next/navigation";
 import Movement from "@/components/workout/Movement";
-export const dynamic = "force-dynamic";
+// export const dynamic = "force-dynamic";
 
 export const generateMetadata = () => {
   return {
