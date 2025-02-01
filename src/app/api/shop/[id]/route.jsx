3401c0fbbb18dd495827d885b808b9b7b4cb123f
youcef23061983 <youcef.belcourtois@gmail.com>
2025-02-01 21:33:43 +0100

    detail commit

--- src/app/api/shop/[id]/route.jsx
+++ src/app/api/shop/[id]/route.jsx
@@ -5,8 +5,11 @@ export async function GET(request, { params }) {
   const { id } = params;
 
   try {
-    const product = Products.find((pro) => pro.id === parseInt(id));
-    return new Response(JSON.stringify(product));
+    // const product = Products.find((pro) => pro.id === parseInt(id));
+    const product = data?.find((pro) => pro.id === id);
+
+    // return new Response(JSON.stringify(product));
+    return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching package details:", error);
     return NextResponse.json(
---
+++ src/app/shop/[id]/page.js
@@ -0,0 +1,84 @@
+// import { BASE_API_URL } from "@/utils/Url";
+// export async function generateStaticParams() {
+//   if (!BASE_API_URL) {
+//     return null;
+//   }
+//   const response = await fetch(`${BASE_API_URL}/api/shop`);
+//   if (!response.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   const posts = await response.json();
+
+//   return posts.map((post) => ({
+//     id: post.id.toString(),
+//   }));
+// }
+
+// const page = async ({ params: { id } }) => {
+//   if (!BASE_API_URL) {
+//     return null;
+//   }
+//   async function getDetailData(id) {
+//     const response = await fetch(`${BASE_API_URL}/api/shop/${id}`);
+//     if (!response.ok) {
+//       throw new Error("Failed to fetch data");
+//     }
+//     return response.json();
+//   }
+
+//   const product = await getDetailData(id);
+//   if (!product) {
+//     return null;
+//   }
+//   return (
+//     <div>
+//       <h1>{product.title}</h1>
+//       <h1>{product.price}</h1>
+//     </div>
+//   );
+// };
+
+// export default page;
+
+import { BASE_API_URL } from "@/utils/Url";
+
+export async function generateStaticParams() {
+  const response = await fetch(`${BASE_API_URL}/api/shop`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const posts = await response.json();
+
+  return posts.map((post) => ({
+    id: post.id,
+  }));
+}
+
+const page = async ({ params: { id } }) => {
+  if (!BASE_API_URL) {
+    return null;
+  }
+
+  async function getDetailData(id) {
+    const response = await fetch(`${BASE_API_URL}/api/shop/${id}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch detail data");
+    }
+    return response.json();
+  }
+
+  try {
+    const data = await getDetailData(id);
+    return (
+      <div>
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Error loading data</div>;
+  }
+};
+
+export default page;
