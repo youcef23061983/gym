2b7d462aedcca2bbab7e6514f0b0bfab40f5daaf
youcef23061983 <youcef.belcourtois@gmail.com>
2025-02-01 21:47:45 +0100

    route commit

--- src/app/api/shop/[id]/route.jsx
+++ src/app/api/shop/[id]/route.jsx
@@ -5,11 +5,11 @@ export async function GET(request, { params }) {
   const { id } = params;
 
   try {
-    // const product = Products.find((pro) => pro.id === parseInt(id));
-    const product = data?.find((pro) => pro.id === id);
+    const product = Products.find((pro) => pro.id === parseInt(id));
+    // const product = data?.find((pro) => pro.id === id);
 
-    // return new Response(JSON.stringify(product));
-    return NextResponse.json(product);
+    return new Response(JSON.stringify(product));
+    // return NextResponse.json(product);
   } catch (error) {
     console.error("Error fetching package details:", error);
     return NextResponse.json(
