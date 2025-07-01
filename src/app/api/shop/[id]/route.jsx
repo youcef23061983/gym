f64aa479e95c9ddf73650533f4e6b81060870862
youcef23061983 <youcef.belcourtois@gmail.com>
2025-07-01 22:50:34 +0100

    prisma commit6

--- src/app/api/shop/[id]/route.jsx
+++ src/app/api/shop/[id]/route.jsx
@@ -1,20 +1,27 @@
 const db = require("../../../../../lib/db.js");
 
+// export async function GET(request, { params }) {
+//   try {
+//     const product = await db.product.findUnique({
+//       where: { id: parseInt(params.id) },
+//     });
+
+//     if (!product) {
+//       return Response.json({ error: "Product not found" }, { status: 404 });
+//     }
+
+//     return Response.json(product);
+//   } catch (error) {
+//     return Response.json(
+//       { error: "Failed to fetch product details" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
 export async function GET(request, { params }) {
-  try {
-    const product = await db.product.findUnique({
-      where: { id: parseInt(params.id) },
-    });
-
-    if (!product) {
-      return Response.json({ error: "Product not found" }, { status: 404 });
-    }
-
-    return Response.json(product);
-  } catch (error) {
-    return Response.json(
-      { error: "Failed to fetch product details" },
-      { status: 500 }
-    );
-  }
+  const product = await db.product.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  return Response.json(product);
 }
