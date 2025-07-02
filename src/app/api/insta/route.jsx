7813630efd8f3f825d0dbde12cc121b2c1292dbd
youcef23061983 <youcef.belcourtois@gmail.com>
2025-07-02 12:21:43 +0100

    prisma commit16

--- src/app/api/insta/route.jsx
+++ src/app/api/insta/route.jsx
@@ -1,20 +1,20 @@
-// const db = require("../../../../lib/db.js");
-// export const dynamic = "force-dynamic"; // Opt out of static generation
+const db = require("../../../../lib/db.js");
+export const dynamic = "force-dynamic"; // Opt out of static generation
 
-// export async function GET() {
-//   try {
-//     const galleryImages = await db.galleryImage.findMany();
-//     return new Response(JSON.stringify(galleryImages), {
-//       status: 200,
-//       headers: { "Content-Type": "application/json" },
-//     });
-//   } catch (error) {
-//     return new Response(
-//       JSON.stringify({ error: "Failed to fetch gallery images" }),
-//       {
-//         status: 500,
-//         headers: { "Content-Type": "application/json" },
-//       }
-//     );
-//   }
-// }
+export async function GET() {
+  try {
+    const galleryImages = await db.galleryImage.findMany();
+    return new Response(JSON.stringify(galleryImages), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ error: "Failed to fetch gallery images" }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  }
+}
