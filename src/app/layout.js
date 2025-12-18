fbffea592579dfd401291c90248c2238ed0467ec
youcef23061983 <youcef.belcourtois@gmail.com>
2025-12-18 10:34:13 +0100

    google search commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,14 @@ import AuthProvider from "./(components)/AuthProvider";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="google-site-verification"
+          content="Cy5rjd1m-zIPtwTUewsO5M53Fse1lD9a9-FB2UFG6Dg"
+        />
+      </head>
+
       <body className="site-wrapper">
         <AuthProvider>
           <Navbar />
