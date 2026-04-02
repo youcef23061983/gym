ce71af999fc87510e79db2a80a25925b180d6501
youcef23061983 <youcef.belcourtois@gmail.com>
2026-04-02 19:06:17 +0100

    back to old shop commit

--- src/app/api/shop/route.jsx
+++ src/app/api/shop/route.jsx
@@ -1,18 +1,18 @@
-const db = require("../../../../lib/db.js");
-export const dynamic = "force-dynamic"; // Required!
+// const db = require("../../../../lib/db.js");
+// export const dynamic = "force-dynamic"; // Required!
 
-// GET all products
-export async function GET() {
-  try {
-    const products = await db.product.findMany();
-    return new Response(JSON.stringify(products), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
-  } catch (error) {
-    return new Response(JSON.stringify({ error: "Failed to fetch products" }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
-  }
-}
+// // GET all products
+// export async function GET() {
+//   try {
+//     const products = await db.product.findMany();
+//     return new Response(JSON.stringify(products), {
+//       status: 200,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: "Failed to fetch products" }), {
+//       status: 500,
+//       headers: { "Content-Type": "application/json" },
+//     });
+//   }
+// }
